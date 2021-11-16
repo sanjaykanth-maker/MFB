41f188868c4f97eba6a33324a69bf46502b2feef
sanjaykanth-maker <79373103+sanjaykanth-maker@users.noreply.github.com>
2021-11-16 11:13:07 +0530

    Update translation.py

--- translation.py
+++ translation.py
@@ -2,31 +2,31 @@ class Translation(object):
     START_TEXT =  """Hi. Thank You for using me.
 /help to know how to use me
 TL;DR, Please send a direct (video) link, and I will try to upload on Telegram.
-© @FMDl_Bot
-Subscribe @thwarikh if you 💔 using this bot.."""
+© @Tele_united_bots
+Subscribe @Tele_united_bots if you 💔 using this bot.."""
     RENAME_403_ERR = "Sorry. You are not permitted to rename this file."
     ABS_TEXT = " Please don't be selfish."
-    UPGRADE_TEXT = """No Exclusive plans Available; more deatails contact @thwarikh"""
+    UPGRADE_TEXT = """No Exclusive plans Available; more deatails contact @Tele_united_bots"""
     FORMAT_SELECTION = "Select the desired format: <a href='{}'>file size might be approximate</a> \nIf you want to set custom thumbnail, send photo before or quickly after tapping on any of the below buttons."
     NOYES_URL = "@GetPublicLinkBot URL detected. Please do not abuse the service!"
     DOWNLOAD_START = "trying to download"
     UPLOAD_START = "trying to upload"
     RCHD_BOT_API_LIMIT = "size greater than maximum allowed size (50MB). Neverthless, trying to upload."
     RCHD_TG_API_LIMIT = "Sorry. But, I cannot upload files greater than 1.5GB due to Telegram API limitations."
-    AFTER_SUCCESSFUL_UPLOAD_MSG = "Please rate me if you find me useful. https://t.me/tlgrmcbot?start=anydl_bot-bot"
+    AFTER_SUCCESSFUL_UPLOAD_MSG = "Please rate me if you find me useful. https://t.me/tele_united_bots"
     NOT_AUTH_USER_TEXT = "Please /upgrade your subscription."
-    NOT_AUTH_USER_TEXT_FILE_SIZE = "Detected File Size: {}. Free Users can only upload: {}\nPlease /upgrade your subscription.\nIf you think this is a bug, please contact @thwarikh"
+    NOT_AUTH_USER_TEXT_FILE_SIZE = "Detected File Size: {}. Free Users can only upload: {}\nPlease /upgrade your subscription.\nIf you think this is a bug, please contact @Tele_united_bots"
     SAVED_CUSTOM_THUMB_NAIL = "Custom video / file thumbnail saved. This image will be used in the video / file."
     DEL_ETED_CUSTOM_THUMB_NAIL = "✅ Custom thumbnail cleared succesfully."
     FF_MPEG_DEL_ETED_CUSTOM_MEDIA = "✅ Media cleared succesfully."
     SAVED_RECVD_DOC_FILE = "Document Downloaded Successfully."
     CUSTOM_CAPTION_UL_FILE =  """✪༺ ──•◈•── ──•◈•──༻✪
 📢Channel :-
-➠@MyFilmBox
+➠@Tele_united_bots
 👥Group :-
-➠@MyFilm_Box
+➠@Tele_united_bots
 📺TV Series :-
-➠@MFBTV
+➠@movies6167
 ✪༺ ──•◈•── ──•◈•──༻✪"""
     NO_CUSTOM_THUMB_NAIL_FOUND = "No Custom ThumbNail found."
     NO_VOID_FORMAT_FOUND = "Can you check the url? I am unable to detect video format from URL. If you think this could be a bug please report on @thwarikh"
@@ -47,7 +47,7 @@ Send /me to know current plan details"""
     REPLY_TO_DOC_GET_LINK = "Reply to a Telegram media to get High Speed Direct Download Link"
     REPLY_TO_DOC_FOR_C2V = "Reply to a Telegram media to convert"
     REPLY_TO_DOC_FOR_RENAME_FILE = "Reply to a Telegram media to /rename with custom thumbnail support"
-    AFTER_GET_DL_LINK = "Direct Link <a href='{}'>Generated</a> valid for {} days.\n© @FMDl_Bot"
+    AFTER_GET_DL_LINK = "Direct Link <a href='{}'>Generated</a> valid for {} days.\n© @Tele_united_bots"
     FF_MPEG_RO_BOT_RE_SURRECT_ED = """Syntax: /trim HH:MM:SS [HH:MM:SS]"""
     FF_MPEG_RO_BOT_STEP_TWO_TO_ONE = "First send /downloadmedia to any media so that it can be downloaded to my local. \nSend /storageinfo to know the media, that is currently downloaded."
     FF_MPEG_RO_BOT_STOR_AGE_INFO = "Video Duration: {}\nSend /clearffmpegmedia to delete this media, from my storage.\nSend /trim HH:MM:SS [HH:MM:SS] to cu[l]t a small photo / video, from the above media."
@@ -57,5 +57,5 @@ Send /me to know current plan details"""
     REPLY_TO_MEDIA_ALBUM_TO_GEN_THUMB = "Reply /generatecustomthumbnail to a media album, to generate custom thumbail"
     ERR_ONLY_TWO_MEDIA_IN_ALBUM = "Media Album should contain only two photos. Please re-send the media album, and then try again, or send only two photos in an album."
     INVALID_UPLOAD_BOT_URL_FORMAT = "URL format is incorrect. make sure your url starts with either http:// or https://. You can set custom file name using the format link | file_name.extension"
-    ABUSIVE_USERS = "You are not allowed to use this bot. If you think this is a mistake, please contact <a href='https://telegram.dog/ShrimadhaVahdamirhS'>@SpEcHlDe</a> to remove this restriction."
-    FF_MPEG_RO_BOT_AD_VER_TISE_MENT = "https://telegram.dog/FFMpegRoBot"
+    ABUSIVE_USERS = "You are not allowed to use this bot. If you think this is a mistake, please contact <a href='@Tele_united_bots'>@Tele_united_bots</a> to remove this restriction."
+    FF_MPEG_RO_BOT_AD_VER_TISE_MENT = "@Tele_united_bots"
